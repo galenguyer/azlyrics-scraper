e07d5d53a37f7c88eea5c31f222daf6c998044dd
Galen Guyer <galen@galenguyer.com>
2020-07-19 15:57:55 -0700

    Parse out lyrics from genius

--- src/genius.py
+++ src/genius.py
@@ -1,5 +1,7 @@
 import argparse
 import sys
+import requests
+from bs4 import BeautifulSoup
 
 
 def eprint(*args, **kwargs):
@@ -9,13 +11,29 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
+def download_url(url: str):
+    """
+    Retrieve the page contents and parse out the lyrics from a given url
+    """
+    if not url.startswith('https://genius.com/'):
+        eprint(f'URL "{url}" does not appear to be a valid genius lyrics url')
+        return None
+    result = requests.get(url)
+    if result.status_code != 200:
+        eprint(f'Status code {result.status_code} for url "{url}" indicates failure')
+        return None
+    parsed_page = BeautifulSoup(result.text, 'html.parser')
+    lyrics = parsed_page.find_all('div', attrs={'class': 'lyrics'})[0].text.strip()
+    print(lyrics)
+
+
 def main():
     parser = argparse.ArgumentParser(description='Scraper for lyrics from genius.com')
     parser.add_argument('-u', '--url', help='Direct URL of a song to download')
     args = parser.parse_args()
 
     if args.url is not None:
-        pass
+        download_url(args.url)
     else:
         eprint('No URL given, doing nothing')
 
