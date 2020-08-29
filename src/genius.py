b552e2ccf410767dff799578a0dbbe2617a72c2b
Galen Guyer <galen@galenguyer.com>
2020-08-28 23:28:53 -0400

    Get most data into a song class for genius

--- src/genius.py
+++ src/genius.py
@@ -1,7 +1,24 @@
 import argparse
+import json
 import sys
 import requests
+from json import JSONEncoder
 from bs4 import BeautifulSoup
+
+
+class Song:
+    def __init__(self, title: str, artist: str, album: str, lyrics: str, url: str):
+        self.title = title
+        self.artist = artist
+        self.album = album
+        #self.year = year
+        self.lyrics = lyrics
+        self.url = url
+
+
+class SongEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
 
 
 def eprint(*args, **kwargs):
@@ -23,9 +40,12 @@ def download_url(url: str):
         eprint(f'Status code {result.status_code} for url "{url}" indicates failure')
         return None
     parsed_page = BeautifulSoup(result.text, 'html.parser')
-    lyrics = parsed_page.find_all('div', attrs={'class': 'lyrics'})[0].text.strip()
-    artist = parsed_page.find_all('a', attrs={'class': 'header_with_cover_art-primary_info-primary_artist'})[0].text.strip()
-    title = parsed_page.find_all('h1', attrs={'class': 'header_with_cover_art-primary_info-title'})[0].text.strip()
+    song_lyrics = parsed_page.find_all('div', attrs={'class': 'lyrics'})[0].text.strip()
+    song_artist = parsed_page.find_all('a', attrs={'class': 'header_with_cover_art-primary_info-primary_artist'})[0].text.strip()
+    song_title = parsed_page.find_all('h1', attrs={'class': 'header_with_cover_art-primary_info-title'})[0].text.strip()
+    song_album = parsed_page.find_all('a', attrs={'class': 'song_album-info-title'})[0].text.strip()
+    song = Song(title=song_title, artist=song_artist, album=song_album, lyrics=song_lyrics, url=url)
+    print(json.dumps(song, indent=4, cls=SongEncoder))
 
 
 def main():
